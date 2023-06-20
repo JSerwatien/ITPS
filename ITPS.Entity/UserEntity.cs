db6f8a3f8829526e651d1ee5227f05f645625557
Joshua Serwatien <josh@greatideafactory.com>
2023-06-20 08:07:24 -0500

    Initial Check-In

---
+++ ITPS.Entity/UserEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace ITPS.Entity
+{
+    public class UserEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+
+
+    }
+}
