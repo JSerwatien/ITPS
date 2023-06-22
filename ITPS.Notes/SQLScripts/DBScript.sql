1609a8f30b45d9abe5c31c497b8419169a3aadfb
Joshua Serwatien <josh@greatideafactory.com>
2023-06-22 08:18:59 -0500

    Fixed UserProfile table columns

--- ITPS.Notes/SQLScripts/DBScript.sql
+++ ITPS.Notes/SQLScripts/DBScript.sql
@@ -19,6 +19,8 @@ CREATE TABLE [dbo].[UserProfile](
 	[UserProfileKey] [int] IDENTITY(1,1) NOT NULL,
 	[FirstName] [varchar](50) NOT NULL,
 	[LastName] [varchar](50) NOT NULL,
+	[UserName] [varchar](50) NOT NULL,
+	[Password] [varchar](50) NOT NULL,
 	[EmailAddress] [varchar](50) NOT NULL,
 	[PhoneNumber] [varchar](50) NULL,
 	[ActiveInd] [tinyint] NULL,
@@ -30,20 +32,17 @@ CREATE TABLE [dbo].[UserProfile](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Index [IX_UserProfile]    Script Date: 6/20/2023 8:17:07 AM ******/
-CREATE NONCLUSTERED INDEX [IX_UserProfile] ON [dbo].[UserProfile]
-(
-	[DepartmentKey] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[Department] ADD  CONSTRAINT [DF_Department_ActiveInd]  DEFAULT ((1)) FOR [ActiveInd]
-GO
+
 ALTER TABLE [dbo].[UserProfile] ADD  CONSTRAINT [DF_UserProfile_ActiveInd]  DEFAULT ((1)) FOR [ActiveInd]
 GO
-ALTER TABLE [dbo].[UserProfile] ADD  CONSTRAINT [DF_UserProfile_CreateDateTime]  DEFAULT (getdate()) FOR [CreateDateTime]
+
+ALTER TABLE [dbo].[UserProfile] ADD  CONSTRAINT [DF_UserProfile_CreateDateTime]  DEFAULT (getdate()) FOR [CreatedDateTime]
 GO
+
 ALTER TABLE [dbo].[UserProfile]  WITH CHECK ADD  CONSTRAINT [FK_UserProfile_Department] FOREIGN KEY([DepartmentKey])
 REFERENCES [dbo].[Department] ([DepartmentKey])
 GO
+
 ALTER TABLE [dbo].[UserProfile] CHECK CONSTRAINT [FK_UserProfile_Department]
 GO
+
