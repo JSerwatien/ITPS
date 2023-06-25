b3ca49db5c95bc3f734a57bc3bf80440b24f70a1
avigayilsperka <avigayil.marcus@gmail.com>
2023-06-25 17:37:13 +0300

    Added StatusEntity and StartUpObjectEntity Classes

---
+++ ITPS.Entity/StartUpObjectEntity.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ITPS.Entity
+{
+    public class StartUpObjectEntity
+    {
+       public List <StatusEntity> Status { get; set; }
+       public List <UserEntity> Users { get; set; }
+       public List <DepartmentEntity> Departments { get; set; } 
+    }
+}
---
+++ ITPS.Entity/StatusEntity.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ITPS.Entity
+{
+    public class StatusEntity
+    {
+        public int StatusCodeKey { get; set; }
+        public int StatusCode { get; set; }
+        public string Status { get; set; }
+    }
+}
