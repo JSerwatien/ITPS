829dbc3481081ce5e98cf6e4ad39863482741998
Michali Loebenberg <michalizak4@gmail.com>
2023-06-26 21:34:04 +0300

    added department entity

---
+++ ITPS.Entity/DepartmentEntity.cs
@@ -0,0 +1,10 @@
+namespace ITPS.Entity
+{
+    public class DepartmentEntity
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int DepartmentId { get; set;}
+    }
+}
