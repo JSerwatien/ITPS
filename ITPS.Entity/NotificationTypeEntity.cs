90debe488d589656fcdbe4599d376fb002773e0f
Michali Loebenberg <michalizak4@gmail.com>
2023-07-19 22:29:34 +0300

    populated notification type table, cretaed sp for alerts, created entity

---
+++ ITPS.Entity/NotificationTypeEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITPS.Entity
+{
+    public class NotificationTypeEntity
+    {
+        public int NotificationTypeKey { get; set; }
+        public string NotificationTypeCode { get; set; }
+        public string Description { get; set; }
+    }
+}
